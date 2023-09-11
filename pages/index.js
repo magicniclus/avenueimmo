0cf1172aa818055ef1003f739504a11c8858a916
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-11 18:22:12 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import React from "react";
+import EstimationLayout from "../components/layout/EstimationLayout";
+import Estimation from "../components/estimation/Estimation";
+import Header from "../components/header/Header";
 
 const Accueil = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <EstimationLayout>
+        <Estimation />
+      </EstimationLayout>
+    </>
+  );
 };
 
 export default Accueil;
