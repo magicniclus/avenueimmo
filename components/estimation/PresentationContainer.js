7e4de866161f8830959c6aa7c27032763c1948f0
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-11 20:15:47 +0200

    Update PresentationContainer.js

--- components/estimation/PresentationContainer.js
+++ components/estimation/PresentationContainer.js
@@ -64,7 +64,7 @@ const PresentationContainer = () => {
       <div>
         <h1 className="lg:text-6xl text-4xl lg:w-11/12 mt-10 lg:mt-3 text-gray-700">
           Estimez votre bien en ligne{" "}
-          <span className="inline bg-gradient-to-r from-blue-300 via-blue-500 to-blue-300 bg-clip-text font-display font-bold tracking-tight text-transparent min-h-max">
+          <span className="inline bg-gradient-to-r from-blue-400 via-blue-500 to-blue-400 bg-clip-text font-display font-bold tracking-tight text-transparent min-h-max">
             gratuitement
           </span>
           .
@@ -87,7 +87,7 @@ const PresentationContainer = () => {
           aria-hidden="true"
         >
           <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#93c5fd] to-[#9089fc] opacity-40 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#93c5fd] to-[#1e40af] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
@@ -95,7 +95,7 @@ const PresentationContainer = () => {
           />
         </div>
         <div
-          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#93c5fd] to-[#9089fc] opacity-40 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#93c5fd] to-[#1e40af] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
           style={{
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
