56cceee2542be84881e723b3bd3843aee43c24ee
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-12 15:19:31 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,25 @@ import EstimationLayout from "../components/layout/EstimationLayout";
 import Estimation from "../components/estimation/Estimation";
 import Header from "../components/header/Header";
 import Breadrumb from "../components/breadCrumb/BreadCrumb";
+import EvaluationBanner from "../components/banner/EvaluationBanner";
+import Faq from "../components/faqs/Faq";
+import ImageBanner from "../components/banner/ImageBanner";
+import Footer from "../components/footer/Footer";
 
 const Accueil = () => {
   return (
     <>
       <Header />
-      <EstimationLayout>
-        <Estimation />
-      </EstimationLayout>
-      <Breadrumb list={["ACCUEIL", "ESTIMATION"]} />
+      <main className="w-full flex justify-center items-center flex-col">
+        <EstimationLayout>
+          <Estimation />
+        </EstimationLayout>
+        <Breadrumb list={["ACCUEIL", "ESTIMATION"]} />
+        <EvaluationBanner />
+        <Faq />
+        {/* <ImageBanner /> */}
+      </main>
+      <Footer />
     </>
   );
 };
