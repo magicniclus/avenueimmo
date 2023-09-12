f322bbe5e5409d282dccfc57234e4d3f5806e750
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-12 18:45:05 +0200

    Update  ClassementEnergetique.js

--- components/estimation/component/ ClassementEnergetique.js
+++ components/estimation/component/ ClassementEnergetique.js
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import SelectWithIcon from "./SelectWithIcon";
 
 const ClassementEnergetique = () => {
   const options = ["A", "B", "C", "D", "E", "F", "G"];
   const dispatch = useDispatch();
-  const primaryColor = useSelector((state) => state?.user?.settings?.fontColor);
-  const secondaryColor = useSelector(
-    (state) => state?.user?.settings?.fontColor2
-  );
 
   const [valueDPE, setValueDPE] = useState(0);
   const [valueGES, setValueGES] = useState(0);
@@ -20,29 +16,16 @@ const ClassementEnergetique = () => {
   }, [valueDPE, valueGES]);
   return (
     <div>
-      <h2
-        className="text-2xl font-light lg:my-0 my-5"
-        style={{ color: secondaryColor }}
-      >
+      <h2 className="text-2xl font-light lg:my-0 my-5 text-gray-700">
         Classement énergetique de votre bien :
       </h2>
       <div className="mt-5">
         <div className="flex flex-col">
-          <h3
-            className="font-light text-sm mb-3"
-            style={{ color: primaryColor }}
-          >
-            DPE{" "}
-          </h3>
+          <h3 className="font-light text-sm mb-3 text-gray-700">DPE </h3>
           <SelectWithIcon options={options} onChange={setValueDPE} />
         </div>
         <div className="flex flex-col mt-5">
-          <h3
-            className="font-light text-sm mb-3"
-            style={{ color: primaryColor }}
-          >
-            GSE{" "}
-          </h3>
+          <h3 className="font-light text-sm mb-3 text-gray-700">GSE </h3>
           <SelectWithIcon options={options} onChange={setValueGES} />
         </div>
       </div>
