858ad1481bbdbbc5224529621fb55e069aff6a18
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-12 18:46:07 +0200

    Create ContainerEstimationTwo.js

---
+++ components/layout/ContainerEstimationTwo.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const ContainerEstimationTwo = (props) => {
+  const stateFontColor = useSelector(
+    (state) => state?.user?.settings?.fontColor
+  );
+  const stateFontColor2 = useSelector(
+    (state) => state?.user?.settings?.fontColor2
+  );
+  return (
+    <div
+      id="estimation"
+      className="lg:w-full sm:w-11/12 w-full max-w-[1250px] min-h-[450px] px-5 py-10 lg:px-20 lg:py-16 flex items-center justify-between flex-col lg:flex-row relative z-20 shadow-lg md:mt-10 rounded-lg"
+    >
+      <div className="absolute -top-1 -right-1 z-20 w-32 h-32 overflow-hidden lg:block hidden"></div>
+      {props.children}
+    </div>
+  );
+};
+
+export default ContainerEstimationTwo;
