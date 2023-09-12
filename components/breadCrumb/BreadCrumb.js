a34002d3b86a99dc6bece73cc4288662f2fafb5d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-12 10:01:19 +0200

    Create BreadCrumb.js

---
+++ components/breadCrumb/BreadCrumb.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Breadrumb = (props) => {
+  const list = props.list;
+
+  const style = "text-blue-500 text-xs font-bold";
+
+  return (
+    <div className="lg:w-full sm:w-11/12 w-full max-w-[1250px] py-10 md:py-16 mx-auto">
+      <div className="px-5">
+        <div className="flex">
+          {list.map((item, idx) => {
+            if (idx < list.length - 1) {
+              return (
+                <p key={idx} className={style}>
+                  <span className="underline underline-offset-2">
+                    {item.toUpperCase()}
+                  </span>{" "}
+                  &gt; &nbsp;{" "}
+                </p>
+              );
+            } else {
+              return (
+                <p key={idx} className={style}>
+                  {item.toUpperCase()}
+                </p>
+              );
+            }
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Breadrumb;
