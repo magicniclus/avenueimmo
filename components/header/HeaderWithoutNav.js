f7509fd253af28f761b6b2b39aa5b4efb443e0c8
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-12 18:58:00 +0200

    Update HeaderWithoutNav.js

--- components/header/HeaderWithoutNav.js
+++ components/header/HeaderWithoutNav.js
@@ -1,15 +1,14 @@
 import React from "react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const HeaderWithoutNav = () => {
   return (
-    <header className="bg-white shadow-sm ">
+    <header className="bg-white shadow-sm">
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between p-6"
         aria-label="Global"
       >
         <div className="flex lg:flex-1 ">
-          <a href="#" className="-m-1.5 p-1.5">
+          <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Avenue-immo</span>
             <img
               className="h-auto w-28"
