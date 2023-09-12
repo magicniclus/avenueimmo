9ac50b3c1568970f4cf08cb9f2cabe1de7dc0de6
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-12 18:57:58 +0200

    Update Header.js

--- components/header/Header.js
+++ components/header/Header.js
@@ -18,7 +18,7 @@ export default function Header() {
         aria-label="Global"
       >
         <div className="flex lg:flex-1 ">
-          <a href="#" className="-m-1.5 p-1.5">
+          <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Avenue-immo</span>
             <img
               className="h-auto w-28"
