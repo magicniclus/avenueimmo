667ee7fa7d932ee59b2fbccbd222ff510a8915d9
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-12 18:45:25 +0200

    Update CardWithoutSize.js

--- components/estimation/component/CardWithoutSize.js
+++ components/estimation/component/CardWithoutSize.js
@@ -5,10 +5,6 @@ const CardWithoutSize = (props) => {
   const { title, selectedCard, setSelectedCard, handleCardChange } = props;
   const typeOfCard = props.typeOfCard || null;
   const standing = useSelector((state) => state?.clientInformation?.standing);
-  const primaryColor = useSelector((state) => state?.user?.settings?.fontColor);
-  const secondaryColor = useSelector(
-    (state) => state?.user?.settings?.fontColor2
-  );
   const isSelected = typeOfCard ? selectedCard : title === selectedCard;
 
   const handleClick = () => {
@@ -25,16 +21,16 @@ const CardWithoutSize = (props) => {
       className={`mr-5 xs:mr-0 relative w-24 py-5 bg-white border rounded-md flex justify-center items-center cursor-pointer transition-all duration-100 hover:shadow-lg mt-2 ${
         isSelected ? "shadow-md" : ""
       }`}
-      style={{ borderColor: secondaryColor }}
+      style={{ borderColor: "#3b82f6" }}
     >
-      <p className="font-light text-sm" style={{ color: secondaryColor }}>
+      <p className="font-light text-sm" style={{ color: "#3b82f6" }}>
         {title}
       </p>
       <div className="w-4 h-4 bg-gray-200 rounded-full absolute top-1 right-1">
         <div
           className="w-3 h-3 rounded-full absolute top-0.5 right-0.5"
           style={{
-            backgroundColor: secondaryColor,
+            backgroundColor: "#3b82f6",
             display: isSelected ? "" : "none",
           }}
         ></div>
