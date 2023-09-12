316d8b38ef23de4b34918f975e5ea2542c668583
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-12 18:45:09 +0200

    Update CardWithSize.js

--- components/estimation/component/CardWithSize.js
+++ components/estimation/component/CardWithSize.js
@@ -1,12 +1,7 @@
 import React from "react";
-import { useSelector } from "react-redux";
 
 const CardWithSize = (props) => {
   const { title, selected, setSelected } = props;
-  const primaryColor = useSelector((state) => state?.user?.settings?.fontColor);
-  const secondaryColor = useSelector(
-    (state) => state?.user?.settings?.fontColor2
-  );
   const handleClick = () => {
     if (selected !== title) {
       setSelected(title);
@@ -21,16 +16,16 @@ const CardWithSize = (props) => {
       className={`mr-5 mt-2  relative w-24 py-5 bg-white border rounded-md flex justify-center items-center cursor-pointer transition-all duration-100 hover:shadow-lg ${
         selected ? "shadow-md" : null
       }`}
-      style={{ borderColor: secondaryColor }}
+      style={{ borderColor: "#3b82f6" }}
     >
-      <p className=" font-light text-sm" style={{ color: secondaryColor }}>
+      <p className=" font-light text-sm" style={{ color: "#3b82f6" }}>
         {title}
       </p>
       <div className="w-4 h-4 bg-gray-200 rounded-full absolute top-1 right-1">
         <div
           className="w-3 h-3 rounded-full absolute top-0.5 right-0.5"
           style={{
-            backgroundColor: secondaryColor,
+            backgroundColor: "#3b82f6",
             display: selected ? "" : "none",
           }}
         ></div>
