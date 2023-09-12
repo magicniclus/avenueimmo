37c05e4fc464c68fa9df64a650dc3879dbc844df
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-12 18:45:31 +0200

    Update InputNumber.js

--- components/estimation/component/InputNumber.js
+++ components/estimation/component/InputNumber.js
@@ -1,16 +1,10 @@
 import React from "react";
-import { useSelector } from "react-redux";
 
 const InputNumber = (props) => {
   const value = props.value;
   const setValue = props.onChange;
   const type = props.type;
   const placeholder = props.placeholder;
-
-  const primaryColor = useSelector((state) => state?.user?.settings?.fontColor);
-  const secondaryColor = useSelector(
-    (state) => state?.user?.settings?.fontColor2
-  );
 
   const handleInputChange = (event) => {
     const inputValue = event.target.value;
@@ -29,13 +23,13 @@ const InputNumber = (props) => {
         value={value}
         step="1"
         onChange={handleInputChange}
-        className="max-w-[500px] w-[80%] sm:w-[90%] border px-4 py-3 rounded-l-md font-light text-sm outline-none"
+        className="max-w-[500px] w-[80%] sm:w-[90%] border px-4 py-3 rounded-l-md font-light text-sm outline-none text-gray-700"
         placeholder={placeholder || "Entrez la surface..."}
-        style={{ borderColor: secondaryColor, color: primaryColor }}
+        style={{ borderColor: "#3b82f6" }}
       />
       <div
         className="w-[20%] sm:w-[10%] flex items-center justify-center rounded-r-md py-2 px-5"
-        style={{ backgroundColor: secondaryColor }}
+        style={{ backgroundColor: "#3b82f6" }}
       >
         <p className="text-white">
           {
