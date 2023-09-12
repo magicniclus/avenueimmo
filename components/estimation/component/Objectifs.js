f0acaf676e8f68f629d86589e8dac4543041dfdb
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-12 18:45:38 +0200

    Update Objectifs.js

--- components/estimation/component/Objectifs.js
+++ components/estimation/component/Objectifs.js
@@ -13,10 +13,6 @@ const Objectifs = () => {
   const optionsContract = ["Propriétaire", "Locataire"];
 
   const dispatch = useDispatch();
-  const primaryColor = useSelector((state) => state?.user?.settings?.fontColor);
-  const secondaryColor = useSelector(
-    (state) => state?.user?.settings?.fontColor2
-  );
   const type = useSelector((state) => state?.clientInfomation?.type);
   const [valueVente, setValueVente] = useState(0);
   const [valueContract, setValueContract] = useState(0);
@@ -29,29 +25,18 @@ const Objectifs = () => {
 
   return (
     <div>
-      <h2
-        className="text-2xl font-light lg:my-0 my-5"
-        style={{ color: secondaryColor }}
-      >
+      <h2 className="text-2xl font-light lg:my-0 my-5">
         Quels sont vos objectifs ?
       </h2>
       <div className="mt-5">
         <div className="flex flex-col">
-          <h3
-            className="font-light text-sm mb-3"
-            style={{ color: primaryColor }}
-          >
+          <h3 className="font-light text-sm mb-3">
             Quand souhaitez vous vendre votre bien ?
           </h3>
           <SelectWithIcon options={optionsVente} onChange={setValueVente} />
         </div>
         <div className="flex flex-col mt-5">
-          <h3
-            className="font-light text-sm mb-3"
-            style={{ color: primaryColor }}
-          >
-            Vous êtes ?
-          </h3>
+          <h3 className="font-light text-sm mb-3 text-gray-700">Vous êtes ?</h3>
           <SelectWithIcon
             options={optionsContract}
             onChange={setValueContract}
