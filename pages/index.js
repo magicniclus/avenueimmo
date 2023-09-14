576c429a894abd078f846ded67bd58ed2fdfbb64
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-14 18:18:00 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,36 @@ import Header from "../components/header/Header";
 import Breadrumb from "../components/breadCrumb/BreadCrumb";
 import EvaluationBanner from "../components/banner/EvaluationBanner";
 import Faq from "../components/faqs/Faq";
-import ImageBanner from "../components/banner/ImageBanner";
 import Footer from "../components/footer/Footer";
 import ServiceBanner from "../components/banner/ServiceBanner";
 import CTABanner from "../components/banner/CTABanner";
+import Head from "next/head";
 
 const Accueil = () => {
   return (
     <>
+      <Head>
+        <title>Avenue-Immo : Estimation Immobilière Gratuite en Ligne</title>
+        <meta
+          name="description"
+          content="Découvrez la valeur de votre bien immobilier en quelques clics avec Avenue-Immo. Notre estimateur en ligne gratuit vous fournit une évaluation précise basée sur les dernières tendances du marché."
+        />
+        <meta
+          name="keywords"
+          content="estimation immobilière, estimation gratuite, estimateur en ligne, valeur immobilière, marché immobilier"
+        />
+        <meta
+          property="og:title"
+          content="Avenue-Immo : Estimation Immobilière Gratuite en Ligne"
+        />
+        <meta
+          property="og:description"
+          content="Découvrez la valeur de votre bien immobilier en quelques clics avec Avenue-Immo. Notre estimateur en ligne gratuit vous fournit une évaluation précise basée sur les dernières tendances du marché."
+        />
+        <meta property="og:image" content="URL_DE_VOTRE_IMAGE_EN_ÉVIDENCE" />
+        <meta property="og:url" content="https://www.avenue-immo.com" />
+        <link rel="canonical" href="https://www.avenue-immo.com" />
+      </Head>
       <Header />
       <main className="w-full flex justify-center items-center flex-col">
         <EstimationLayout>
@@ -23,7 +45,6 @@ const Accueil = () => {
         <ServiceBanner />
         <Faq />
         <CTABanner />
-        {/* <ImageBanner /> */}
       </main>
       <Footer />
     </>
