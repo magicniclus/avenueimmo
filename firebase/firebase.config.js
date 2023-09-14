c716b1583a4e33cadb725cec4b58d068b8d9e74d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-14 13:55:55 +0200

    Update firebase.config.js

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -1,8 +1,8 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: "AIzaSyB5lsXii0xKUITHBb8xRKs0qCMtNjHSHgM",
   authDomain: "avenue-immo.firebaseapp.com",
   databaseURL:
     "https://avenue-immo-default-rtdb.europe-west1.firebasedatabase.app",
@@ -12,8 +12,13 @@ const firebaseConfig = {
   appId: "1:1095138669993:web:124e0ff01a333b191d80b8",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Vérifier si Firebase a déjà été initialisé
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApps()[0];
+}
 
 // Get a reference to the auth service
 const auth = getAuth();
