3f33a392dd1219b61cb7e60d29925eddaabff071
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-07 16:30:44 +0100

    Create Sart.js

---
+++ components/pro/etapes/Sart.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Sart = () => {
+  return (
+    <>
+      <h1 className="text-center lg:text-6xl text-4xl lg:w-11/12 mt-10 lg:mt-3 text-gray-700">
+        Postulez pour devenir partenaire{" "}
+        <span className="inline bg-gradient-to-r from-blue-500 via-blue-300 to-blue-500 bg-clip-text font-display font-bold tracking-tight text-transparent min-h-max">
+          exclusif
+        </span>{" "}
+        sur{" "}
+        <span className="inline bg-gradient-to-r from-blue-500 via-blue-300 to-blue-500 bg-clip-text font-display font-bold tracking-tight text-transparent min-h-max">
+          votre secteur
+        </span>
+      </h1>
+      <h2 className="text-center mt-10 text-xl">
+        Nous recrutons un <span className="font-bold">unique partenaire</span>{" "}
+        par <span className="font-bold">ville de France</span>.
+      </h2>
+    </>
+  );
+};
+
+export default Sart;
