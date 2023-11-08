7d737965adbc5045a14868a3a85a6611ef0d92a1
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-08 17:30:24 +0100

    Update Pourquoi.js

--- components/pro/etapes/Pourquoi.js
+++ components/pro/etapes/Pourquoi.js
@@ -18,13 +18,13 @@ const Pourquoi = () => {
       <h1 className="text-center lg:text-4xl text-2xl lg:w-11/12 mt-10 lg:mt-3 text-gray-700">
         Pourquoi devrions-nous travailler ensemble ?
       </h1>
-      <h2>
-        Nous avons un unique partenaire par secteur, nous devons donc
-        selectionner avec minutie notre partanaire afin de répondre à la
-        demande.
+      <h2 className=" lg:w-11/12 text-center mt-3">
+        Nous avons un unique partenaire par secteur, nous devons donc le
+        selectionner avec minutie afin de répondre à la forte demande de nos
+        clients.
       </h2>
       <div className="mt-7">
-        {/* <TextArea placeholder="Agence" value={value} onChange={setValue} /> */}
+        <TextArea placeholder="" value={value} onChange={setValue} />
       </div>
     </>
   );
