ded6a8977640bc819686fb37e8e8d4c63479d119
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-08 15:39:59 +0100

    Update Secteur.js

--- components/pro/etapes/Secteur.js
+++ components/pro/etapes/Secteur.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import Input from "../Input";
 
 const Secteur = () => {
-  return <div></div>;
+  const [value, setValue] = useState("");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({
+      type: "SET_PRO_INFORMATION",
+      payload: { secteur: value },
+    });
+  }, [value]);
+
+  return (
+    <>
+      <h1 className="text-center lg:text-4xl text-2xl lg:w-11/12 mt-10 lg:mt-3 text-gray-700">
+        Quel est votre secteur d'activité ?
+      </h1>
+      <div className="mt-7">
+        <Input
+          placeholder="Entrez un code postal"
+          type="CP"
+          value={value}
+          onChange={setValue}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Secteur;
